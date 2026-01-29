a854360aa8c45f351a09ab46227e505a58ff5f42
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2026-01-29 12:49:41 +0000

    Lesson04

--- Lesson Exercises/CT08_03/project3_restaurantmenu.py
+++ Lesson Exercises/CT08_03/project3_restaurantmenu.py
@@ -27,55 +27,50 @@ def ask_for_food():
     wallet=10
     while True:
         question=input("What would you like to order? ")
-        
-        if question in menu:
-            # print("The "+question+" is $"+str(Menu[question]))
+        if question[0:6] == "remove":
+            if question[7:] not in order:
+                print("You do not have that in your order")
+            else:
+                del order[question[7:]]
+                print(f"Your {question[7:]} has been removed")
+                wallet += menu[question[7:]]
+                print(f"Your balance is now ${wallet:.2f}")
+        elif question in menu:
+                # print("The "+question+" is $"+str(Menu[question]))
             print(f"The {question} is ${menu[question]:.2f}")
             question2=input("Do you want to add it to your order? (y/n)")
-            if question2 =="y":
-                if wallet >= menu[question]:
-                    if question in order:
-                        order[question]+=menu[question]
-                        # print(f"{question} has been added to your order.")
-                        # wallet-=menu[question]
-                        # print(f"Remaining wallet balance : ${wallet:.2f}")
+            if question2 == "y":
+                    if wallet >= menu[question]:
+                        if question in order:
+                            order[question]+=menu[question]
+                            # print(f"{question} has been added to your order.")
+                            # wallet-=menu[question]
+                            # print(f"Remaining wallet balance : ${wallet:.2f}")
+                        else:
+                            order[question]=menu[question]
+                        print(f"{question} has been added to your order.")
+                        wallet -= menu[question]
+                        print(f"Remaining wallet balance : ${wallet:.2f}")
+                        cost=cost+menu[question]
+                        continue
                     else:
-                        order[question]=menu[question]
-                    print(f"{question} has been added to your order.")
-                    wallet-=menu[question]
-                    print(f"Remaining wallet balance : ${wallet:.2f}")
-                    cost=cost+menu[question]
-                    continue
-                else:
-                    print(f"{question} cost ${order[question]:.2f}. You only have{wallet:.2f} in your wallet.")
-                    print(f"{question} not added")
-                # check for whether the key is already in the dict
-                # if question in Order:
-                #     Order[question]+=Menu[question]
-            #     else:
-            #         Order[question]=Menu[question]
-            #     print(question+" has been added to ur order")
-            #     Cost=Cost+Menu[question]
-            #     # print(Cost)
-            #     continue
-            # elif question2== "n":
-            #     print(question+" has not been added to ur order")
-            #     continue
+                        print(f"{question} cost ${order[question]:.2f}. You only have{wallet:.2f} in your wallet.")
+                        print(f"{question} not added")
 
-        elif question == "no more":
-            print("Ok, Thank you for your order!") 
-            print("---Order Summary---")
-            for food2,price2 in order.items():
-                print(f"{food2}:${price2:.2f}")
-            print("-------------------")
-            # print("Total: "+str(Cost))
-            print(f"Total: ${cost:.2f}")
-            # print("Your total bill is "+f"${Cost:.2f}"+" Enjoy your meal!")
-            print(f"Your total bill is ${cost:.2f} Enjoy your meal!")
+            elif question == "no more":
+                print("Ok, Thank you for your order!") 
+                print("---Order Summary---")
+                for food2,price2 in order.items():
+                    print(f"{food2}:${price2:.2f}")
+                print("-------------------")
+                # print("Total: "+str(Cost))
+                print(f"Total: ${cost:.2f}")
+                # print("Your total bill is "+f"${Cost:.2f}"+" Enjoy your meal!")
+                print(f"Your total bill is ${cost:.2f} Enjoy your meal!")
 
-                
-            break
-        else:
-            print("We don't have that item")
-            continue
+                    
+                break
+            else:
+                print("We don't have that item")
+                continue
 ask_for_food()
--- Lesson Exercises/CT08_04/project4_bookOS.py
+++ Lesson Exercises/CT08_04/project4_bookOS.py
@@ -1,44 +1,85 @@
 menu = {
-    "notebook" : 2.50,
-    "pencil" : 0.50,
-    "pen" : 1.20,
-    "ruler" : 1.50,
-    "eraser" : 0.50,
-    "writing pad" : 2.50,
-    "glue" : 3.00,
-    "calculator" : 35.00
+    "notebook" : {"amount":0,"price":2.50},
+    "pencil" : {"amount":0,"price":0.50},
+    "pen" : {"amount":0,"price":1.20},
+    "ruler" : {"amount":0,"price":1.50},
+    "eraser" : {"amount":0,"price":0.50},
+    "writing pad" : {"amount":0,"price":2.50},
+    "glue" : {"amount":0,"price":3.00},
+    "calculator" : {"amount":0,"price":35.00}
+}
+order = {
+
 }
 
+wallet = 50
+cost = 0
 
 print("Welcome to the Bookshop")
 print("Ordering System (BOS)!")
 
 print("\nHere's our menu")
-for item , price in menu.items():
-    print(f"{item}: ${price:.2f}")
+for item , inner in menu.items():
+    print(f"{item}: ${inner["price"]:.2f}")
 
 def ask_for_order():
+    global wallet
+    global cost
     while True:
         item = input("What would you like to buy?").lower().strip()
-    # exit the loop
         if item == "no more":
+            print("----------------------")
+            for item , inner in order.items():
+                print(f"{item}: {inner["amount"]}, ${inner["price"]:.2f}")
+            print(f"Your total is ${cost:.2f}")
+            print("----------------------")
+            print(f"You have ${wallet} left.")
             break
+        if item in menu:# the code that didnt work below
+            print(f"{item.title()} costs ${menu[item]:.2f}")
+            confirm_order = input(f"Do you want to add {item} to your order?(y/n)")
+            if confirm_order == "y":
+                if wallet >= menu[item]:
+                    if item in order:
+                        menu[item] += {"amount"+ 1}
+                        order[item] += menu[item["amount" and "price"]]
+                        cost += menu[item]
+                        wallet -= menu[item]
+                    else:
+                        order[item] = menu[item]
+                        wallet -= menu[item]
+                        print(f"You have ${wallet:.2f} remaining.")
+                        cost += order[item]
+                else:
+                    print("You do not have enough money")
+                    print(f"You have ${wallet:.2f}.")
+            elif confirm_order =="n":
+                print(f"{item} was not added to you order.")
+
+        else:
+            print(f"Sorry we don't have {item} in our store")
+            continue
+
+
+
+        
+ask_for_order()
+
+
+
+
+
+
+
+
 # What would you like to buy? Notebook
 # Notebook costs $2.50.
 
 # What would you like to buy? Chopsticks
 # Sorry, we don’t have Chopsticks in our store.
 
-        if item in menu:
-            print(f"{item.title()} costs ${menu[item]:.2f}")
-            confirm_order = input("Do ")
-        else:
-            print(f"Sorry we don't have {item} in our store")
-            continue
-        
 # Do you want to add Notebook to your order? (y/n): y
 # Notebook has been added to your order.
 
 # Do you want to add Pencil to your order? (y/n): n
 # Pencil was not added to your order.
-ask_for_order()
